fc4cb8e64b28de8fd734cbfec885100179437fda
Manuel <97695681+manudelu@users.noreply.github.com>
2024-05-24 18:44:09 +0200

    FInalaaa

---
+++ Final/maps/binvox_generator.py
@@ -0,0 +1,10 @@
+import airsim
+import os
+
+def binvox_generator():
+    c = airsim.VehicleClient()
+    center = airsim.Vector3r(0, 0, 0)
+    output_path = os.path.join(os.getcwd(), "new_map.binvox")
+    c.simCreateVoxelGrid(center, 56, 56, 56, 1, output_path)
+
+# In order to visualize the .binvox install viewvox from this link: https://www.patrickmin.com/viewvox/
