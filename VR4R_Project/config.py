82530abd8084e55753c2023e2a3cae93ff08d228
Manuel <97695681+manudelu@users.noreply.github.com>
2024-07-04 21:52:24 +0200

    Update config.py

--- VR4R_Project/config.py
+++ VR4R_Project/config.py
@@ -1,4 +1,4 @@
 # Common parameters for all drones
 home = (0, 0)
-target_altitude = -5  # Target altitude (NED coordinates, so negative is up)
+target_altitude = -4  # Target altitude (NED coordinates, so negative is up)
 obstacle_threshold = 4  # Obstacle height threshold
